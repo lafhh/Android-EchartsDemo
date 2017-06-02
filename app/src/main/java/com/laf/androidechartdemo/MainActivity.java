6a0fdfeb5c9af5d6de10cb6b8bce29694ee2d5a2
root <654437674@qq.com>
2017-06-02 18:13:07 +0800

    first scroll data table

--- app/src/main/java/com/laf/androidechartdemo/MainActivity.java
+++ app/src/main/java/com/laf/androidechartdemo/MainActivity.java
@@ -2,16 +2,28 @@ package com.laf.androidechartdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.webkit.WebView;
+import android.widget.ScrollView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static android.media.CamcorderProfile.get;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mWebView;
+    private TableLayout tableLayout;
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_echart);
+        setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.webView);
         mWebView.getSettings().setJavaScriptEnabled(true);
@@ -21,5 +33,31 @@ public class MainActivity extends AppCompatActivity {
         //2.怎么拿到html里面的按钮?
 //        mWebView.addJavascriptInterface(new JsObject(), "injectedObject");
         mWebView.loadUrl("file:///android_asset/jsWeb/main.html");
+
+        tableLayout = (TableLayout) findViewById(R.id.tablelayout);
+        scrollView = (ScrollView) findViewById(R.id.scrollview);
+        initData();
+    }
+
+    private void initData() {
+        int row = 10;
+        int column = 5;
+        TableLayout table = new TableLayout(this);
+        table.setShrinkAllColumns(true);
+        table.setStretchAllColumns(true);
+        scrollView.addView(table);
+        for (int i = 0; i < row; i++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setGravity(Gravity.CENTER);
+            for (int j = 0; j < column; j++) {
+                String content = "col" + (i + 1) + "," + (j + 1);
+                TextView txtView = new TextView(this);
+                txtView.setGravity(Gravity.CENTER);
+                txtView.setText(content);
+                tableRow.addView(txtView);
+            }
+            table.addView(tableRow);
+        }
     }
 }
+
